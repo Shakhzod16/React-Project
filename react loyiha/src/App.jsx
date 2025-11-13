7c677bf80a4ca0d890534321af639eb8a46663d4
Shakhzod16 <qosimov0805@gmail.com>
2025-11-13 17:39:28 +0500

    loyiha

--- react loyiha/src/App.jsx
+++ react loyiha/src/App.jsx
@@ -1,7 +1,24 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-	return <></>;
+	const [count, setCount] = useState(0);
+
+	function increase() {
+		setCount(count + 1);
+	}
+
+	function decrease() {
+		setCount(count - 1);
+	}
+
+	return (
+		<div style={{ textAlign: 'center', marginTop: '50px' }}>
+			<h1>Hisoblagich: {count}</h1>
+			<button onClick={increase}>➕ Qo‘shish</button>
+			<button onClick={decrease}>➖ Ayirish</button>
+		</div>
+	);
 }
 
 export default App;
